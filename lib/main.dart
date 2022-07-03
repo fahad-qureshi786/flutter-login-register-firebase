9a380b8550d6df6ed188658e9cc44c56e0ff1143
fahad-qureshi786 <mfahad.bses19@iba-suk.edu.pk>
2022-07-03 15:51:35 +0500

    disable firebase from application

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_signin/screens/screens/signin_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttersignuploginfirebase/screens/signin.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SignIn(),
+      home: const SignInScreen(),
     );
   }
 }
