0d521aff3ec632833d74ab204fd819b0ab779331
fahad-qureshi786 <mfahad.bses19@iba-suk.edu.pk>
2022-07-04 02:18:00 +0500

    comment signup option

--- lib/screes/signin.dart
+++ lib/screes/signin.dart
@@ -1,6 +1,11 @@
-import 'package:flutter/material.dart';
+import 'dart:js';
 
+import 'package:firebaseflutterlogin/screes/signup.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../reuseable/reuseable.dart';
 import '../utils/colors.dart';
+import 'home.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
@@ -10,6 +15,9 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController _emailTextController = TextEditingController();
+  TextEditingController _passwordTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,10 +32,34 @@ class _SignInScreenState extends State<SignInScreen> {
             ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
         child: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.fromLTRB(
-                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            padding: EdgeInsets.fromLTRB(20, MediaQuery.of(context).size.height * 0.2, 20, 0),
             child: Column(
-              children: <Widget>[],
+              children: <Widget>[
+                logoWidget("assets/images/craib.png"),
+                //text field(placeholder, icon, type:password?, controllerName)
+                reusableTextField("Enter Email", Icons.person_outline, false, _emailTextController),
+                const SizedBox(height: 20,),
+                reusableTextField("Password", Icons.lock_outline, true, _passwordTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                firebaseUIButton(context, "Sign In", () {
+                  FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                      email: _emailTextController.text,
+                      password: _passwordTextController.text)
+                      .then((value) {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                  }).onError((error, stackTrace) {
+                    print("Error ${error.toString()}");
+                  });
+                }),
+                const SizedBox(
+                  height: 5,
+                ),
+                // signUpOption()
+              ],
             ),
           ),
         ),
@@ -35,3 +67,23 @@ class _SignInScreenState extends State<SignInScreen> {
     );
   }
 }
+
+// Row signUpOption() {
+//   return Row(
+//     mainAxisAlignment: MainAxisAlignment.center,
+//     children: [
+//       const Text("Don't have account?",
+//           style: TextStyle(color: Colors.white70)),
+//       GestureDetector(
+//         onTap: () {
+//           Navigator.push(context,
+//               MaterialPageRoute(builder: (context) => SignUpScreen()));
+//         },
+//         child: const Text(
+//           " Sign Up",
+//           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+//         ),
+//       )
+//     ],
+//   );
+// }
--- lib/screes/signup.dart
+++ lib/screes/signup.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SignUpScree extends StatefulWidget {
-  const SignUpScree({Key? key}) : super(key: key);
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
 
   @override
-  State<SignUpScree> createState() => _SignUpScreeState();
+  State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-class _SignUpScreeState extends State<SignUpScree> {
+class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Container();
