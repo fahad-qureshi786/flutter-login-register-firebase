aa809f5a3e18dd0aebaf6771f3b7ddfebca3f3b7
fahad-qureshi786 <mfahad.bses19@iba-suk.edu.pk>
2022-07-03 16:59:27 +0500

    add background linear gredient colors

--- lib/screes/signin.dart
+++ lib/screes/signin.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/colors.dart';
+
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
 
@@ -10,8 +12,26 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              hexStringToColor("000000"),
+              hexStringToColor("6e2945"),
+              hexStringToColor("9598a3")
+            ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(
+              children: <Widget>[],
+            ),
+          ),
+        ),
+      )
     );
   }
 }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-hexStringToColor(String hexColor){
+hexStringToColor(String hexColor) {
   hexColor = hexColor.toUpperCase().replaceAll("#", "");
-  if(hexColor.length==0){
-    hexColor = "FF"+hexColor;
+  if (hexColor.length == 6) {
+    hexColor = "FF" + hexColor;
   }
   return Color(int.parse(hexColor, radix: 16));
 }
